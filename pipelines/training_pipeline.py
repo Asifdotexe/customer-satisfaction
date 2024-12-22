3b91c95ae57fcfa5f35c416c77c8e882a9f7f497
Asif Sayyed <asifdotexe@gmail.com>
2024-12-22 19:49:54 +0530

    #1 added docstring for training_pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -4,8 +4,13 @@ from steps.data_clean import clean_data
 from steps.model_train import train_model
 from steps.model_evaluate import evaluate_model
 
-@pipeline
+@pipeline(enable_cache=True)
 def training_pipeline(data_path: str):
+    """Calling the training pipeline
+
+    Args:
+        data_path (str): Path to the data
+    """
     df = ingest_data(data_path)
     clean_data(df)
     train_model(df)
