150a82767c09bed1c634dd4f4f22a58d9967b60a
Asif Sayyed <asifdotexe@gmail.com>
2024-12-22 16:34:42 +0530

    #1 added an ingest step

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -3,6 +3,9 @@ import pandas as pd
 from zenml import step
 
 class IngestData:
+    """
+    Ingesting data from the data_path
+    """
     def __init__(self, data_path: str):
         """Initialize the IngestData class with the data path."""
         self.data_path = data_path
@@ -11,3 +14,18 @@ class IngestData:
         """Load data from the specified file and log the ingestion process."""
         logging.info(f"Ingesting data from {self.data_path}")
         return pd.read_csv(self.data_path)
+    
+@step
+def ingest_data(data_path: str) -> pd.DataFrame:
+    """Ingesting the data from the data_path
+    
+    :params data_path: path to the data file
+    :return: pd.DataFrame containing the loaded data
+    """
+    try:
+        ingest_data = IngestData(data_path=data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(f"Error ingesting data: {e}")
+        raise e
