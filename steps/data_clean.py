b5010db485813e54e37aa05c6c8372e8d5fd8ac7
Asif Sayyed <asifdotexe@gmail.com>
2024-12-22 19:25:04 +0530

    #1 updated the template function to return none as it was resulting in error

--- steps/data_clean.py
+++ steps/data_clean.py
@@ -3,7 +3,7 @@ import pandas as pd
 from zenml import step
 
 @step
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_data(df: pd.DataFrame) -> None:
     """Cleans the ingested data
 
     Args:
