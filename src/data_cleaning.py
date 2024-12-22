1c0fbe41b1c3b39b0b5c469e10119fe88d0c27af
Asif Sayyed <asifdotexe@gmail.com>
2024-12-22 21:37:29 +0530

    #1 implemented DataPreProcessingStrategy template

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -10,3 +10,16 @@ class DataStrategy(ABC):
     @abstractmethod
     def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
         pass
+    
+class DataPreProcessStrategy(DataStrategy):
+    """Strategy for preprocessing data"""
+    def handle(self, data: pd.DataFrame) -> pd.DataFrame:
+        """Preprocess data
+
+        Args:
+            data (pd.DataFrame): Pandas dataframe containing ingested dataset
+
+        Returns:
+            pd.DataFrame: Pandas dataframe containing preprocessed dataset
+        """
+        logging.info("Preprocessing data")
