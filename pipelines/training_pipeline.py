6e62b62d3297637522c6a0482af7df28fda6392c
Asif Sayyed <asifdotexe@gmail.com>
2024-12-22 19:25:58 +0530

    #1 fixed the typo of pipeline being pipelines

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,10 +1,10 @@
-from zenml import pipelines
+from zenml import pipeline
 from steps.data_ingest import ingest_data
 from steps.data_clean import clean_data
 from steps.model_train import train_model
 from steps.model_evaluate import evaluate_model
 
-@pipelines()
+@pipeline
 def training_pipeline(data_path: str):
     df = ingest_data(data_path)
     clean_data(df)
