53259b2bad46edf124eca367841bcf36b06aacc2
Asif Sayyed <asifdotexe@gmail.com>
2024-12-23 15:34:59 +0530

    #1 fixed the syntax error

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -63,7 +63,7 @@ class DataSplitStrategy(DataStrategy):
             X = data.drop(['review_score'], axis=1)
             # y contains the dependent variable
             y = data['review_score']
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2 random_state=42)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             return X_train, X_test, y_train, y_test
         except Exception as e:
             logging.error(f"Error in splitting data: {e}")
