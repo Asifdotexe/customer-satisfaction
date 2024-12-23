697f42cb6a5fd42875fc30f84636d6007da13903
Asif Sayyed <asifdotexe@gmail.com>
2024-12-23 19:13:24 +0530

    #1 implemented train_model function

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,12 +1,35 @@
 import logging
 import pandas as pd
 from zenml import step
+from sklearn.base import RegressionMixin
+
+from src.config import ModelNameConfig
+from src.model_development import LinearRegressionModel
 
 @step
-def train_model(df: pd.DataFrame) -> None:
-    """Trains the model on the ingested data
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.Series,
+    y_train: pd.DataFrame,
+    y_test: pd.Series,
+    config: ModelNameConfig
+) -> RegressionMixin:
+    """Trains the machine learning model
 
     Args:
-        df (pd.DataFrame): pandas dataframe containing the ingested data
+        X_train (pd.DataFrame): pandas Dataframe containing  the training data
+        X_test (pd.Series): pandas Series containing the testing data
+        y_train (pd.DataFrame): pandas Dataframe containing the training labels
+        y_test (pd.Series): pandas Series containing the testing labels
+        config (ModelNameConfig): Model configuration containing the model configuration
+
+    Returns:
+        RegressionMixin: Trained model
     """
-    pass
+    model = None
+    if config.model_name == "LinearRegression":
+        model = LinearRegressionModel()
+        trained_model = model.train(X_train, y_train)
+        return trained_model
+    else:
+        raise ValueError("Invalid model")
