f71e3984f73d72d7fb93c2076edb5ebfca80ac4c
Asif Sayyed <asifdotexe@gmail.com>
2024-12-23 15:50:47 +0530

    #1 implemented the clean_df function

--- steps/data_clean.py
+++ steps/data_clean.py
@@ -2,14 +2,36 @@ import logging
 import pandas as pd
 from zenml import step
 
+from typing import Annotated
+from src.data_cleaning import DataCleaning, \
+    DataPreProcessStrategy, DataSplitStrategy
+
 @step
-def clean_df(df: pd.DataFrame) -> None:
+def clean_df(df: pd.DataFrame) -> tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.DataFrame, "y_train"],
+    Annotated[pd.DataFrame, "y_test"],
+]:
     """Cleans the ingested data
 
     Args:
         df (pd.DataFrame): pandas Dataframe containing the ingested data
 
     Returns:
-        pd.DataFrame: pandas Dataframe containing the cleaned data
+        X_train (pd.DataFrame): Training data
+        X_test (pd.DataFrame): Testing data
+        y_train (pd.Series): Training labels
+        y_test (pd.Series): Testing labels
     """
-    pass
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+        
+        split_strategy = DataSplitStrategy()
+        data_cleaning = DataCleaning(process_strategy, split_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+    except Exception as e:
+        logging.info(f"Error in cleaning data: {e}")
+        raise e
