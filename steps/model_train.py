8f1d637fe5665f9007749403804d75bdf21103e3
Asif Sayyed <asifdotexe@gmail.com>
2024-12-25 19:25:36 +0530

    #1 removed the test features and labels from input as they aren't needed for training

--- steps/model_train.py
+++ steps/model_train.py
@@ -9,18 +9,14 @@ from src.strategy_model_development import LinearRegressionModel
 @step
 def train_model(
     X_train: pd.DataFrame,
-    X_test: pd.Series,
     y_train: pd.DataFrame,
-    y_test: pd.Series,
     config: ModelNameConfig
 ) -> RegressorMixin:
     """Trains the machine learning model
 
     Args:
         X_train (pd.DataFrame): pandas Dataframe containing  the training data
-        X_test (pd.Series): pandas Series containing the testing data
         y_train (pd.DataFrame): pandas Dataframe containing the training labels
-        y_test (pd.Series): pandas Series containing the testing labels
         config (ModelNameConfig): Model configuration containing the model configuration
 
     Returns:
@@ -28,6 +24,7 @@ def train_model(
     """
     model = None
     if config.model_name == "LinearRegression":
+        logging.info("Training Linear Regression model")
         model = LinearRegressionModel()
         trained_model = model.train(X_train, y_train)
         return trained_model
