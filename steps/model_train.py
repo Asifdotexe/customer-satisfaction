04194eedbac6e3ab589280c9c2bd983ff5d8430d
Asif Sayyed <asifdotexe@gmail.com>
2024-12-25 20:56:48 +0530

    #3 debugged the pipeline

--- steps/model_train.py
+++ steps/model_train.py
@@ -3,27 +3,26 @@ import pandas as pd
 from zenml import step
 from sklearn.base import RegressorMixin
 
-from src.config import ModelNameConfig
+from src.config import ACTIVE_MODEL_NAME
 from src.strategy_model_development import LinearRegressionModel
 
 @step
 def train_model(
     X_train: pd.DataFrame,
     y_train: pd.DataFrame,
-    config: ModelNameConfig
 ) -> RegressorMixin:
     """Trains the machine learning model
 
     Args:
         X_train (pd.DataFrame): pandas Dataframe containing  the training data
         y_train (pd.DataFrame): pandas Dataframe containing the training labels
-        config (ModelNameConfig): Model configuration containing the model configuration
+        config (str): String specifying the model name
 
     Returns:
         RegressorMixin: Trained model
     """
     model = None
-    if config.model_name == "LinearRegression":
+    if ACTIVE_MODEL_NAME == "LinearRegression":
         logging.info("Training Linear Regression model")
         model = LinearRegressionModel()
         trained_model = model.train(X_train, y_train)
