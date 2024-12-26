de70ad978388e0e82d729f56183f09aad01dd03b
Asif Sayyed <asifdotexe@gmail.com>
2024-12-26 21:07:48 +0530

    #6 added experimental tracker using zenml and logging using mlflow

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,12 +1,17 @@
 import logging
+
+import mlflow
 import pandas as pd
 from zenml import step
+from zenml.client import Client
 from sklearn.base import RegressorMixin
 
 from src.config import ACTIVE_MODEL_NAME
 from src.strategy_model_development import LinearRegressionModel
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(
     X_train: pd.DataFrame,
     y_train: pd.DataFrame,
@@ -23,6 +28,7 @@ def train_model(
     """
     model = None
     if ACTIVE_MODEL_NAME == "LinearRegression":
+        mlflow.sklearn.autolog()
         logging.info("Training Linear Regression model")
         model = LinearRegressionModel()
         trained_model = model.train(X_train, y_train)
